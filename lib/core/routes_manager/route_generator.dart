d2b015b08a46e5e019c4532e9d49016e2d0db6c0
Mohamed Shaban <mohamed.shabanzakaryia@gmail.com>
2026-05-11 03:30:10 +0300

    + feature ui

---
+++ lib/core/routes_manager/route_generator.dart
@@ -0,0 +1,46 @@
+import 'package:ecommerce/core/routes_manager/routes.dart';
+import 'package:ecommerce/features/auth/presentation/screens/sign_in_screen.dart';
+import 'package:ecommerce/features/auth/presentation/screens/sign_up_screen.dart';
+import 'package:ecommerce/features/cart/screens/cart_screen.dart';
+import 'package:ecommerce/features/main_layout/main_layout.dart';
+import 'package:ecommerce/features/product_details/presentation/screen/product_details.dart';
+import 'package:ecommerce/features/products_screen/presentation/screens/products_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+
+      case Routes.cartRoute:
+        return MaterialPageRoute(builder: (_) => const CartScreen());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainLayout());
+
+      case Routes.productsScreenRoute:
+        return MaterialPageRoute(builder: (_) => const ProductsScreen());
+
+      case Routes.productDetails:
+        return MaterialPageRoute(builder: (_) => const ProductDetails());
+
+      case Routes.signInRoute:
+        return MaterialPageRoute(builder: (_) => const SignInScreen());
+
+      case Routes.signUpRoute:
+        return PageTransition(type: PageTransitionType.fade ,child: SignUpScreen());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('No Route Found'),
+        ),
+        body: const Center(child: Text('No Route Found')),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:ecommerce/core/routes_manager/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/di/di.dart';
+import 'core/routes_manager/route_generator.dart';
+
+void main() {
+
+  configureDependencies();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: child,
+        onGenerateRoute: RouteGenerator.getRoute,
+        initialRoute: Routes.signInRoute,
+      ),
+    );
+  }
+}
